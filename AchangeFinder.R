5da8250d3b682458a5a017b001057773d5c4452e
Andy Krause <alkrause@uw.edu>
2013-11-12 21:44:36 -0800

    AchangeFinder: Updating Syntax

--- AchangeFinder.R
+++ AchangeFinder.R
@@ -1,20 +1,43 @@
+################################################################################                                                                                                      ###                                                                          ###  
+#                                                                              #
+#    LUCIA MODEL: AchangeFinder                                                #
+#                                                                              #
+#        Locates Changes to Apartment Record Type Data                         #                                                                                    ###          by Andy Krause                                                  ###
+#                                                                              #
+#        Most Recent Update: 11/2/2013                                         #
+#                                                                              # 
+###############################################################################'
 
-AchangeFinder <- function(X,Field,Beg.Year,End.Year, rev=F){
- 
+AchangeFinder <- function(X, Field, Beg.Year, End.Year, rev=F){
+
+# 1.0 Prep Data ----------------------------------------------------------------
+
+ # 1.1 Set sequence of years
  Ys <- Beg.Year:End.Year  
+
+ # 1.2 Set up blank results
  Results <- rep(0,length(X[,1]))
+ 
+ # 1.3 Isolate PIN numbers
  PX <- as.data.frame(X$PINX)
  colnames(PX) <- "PINX"
+ 
+ # 1.4 Set up Length
  yl <- dim(PX)[1]
 
+ # 1.5 Set Field
  a.Field <- "Field"
  if(Field == "RecType"){
    a.Field <- "RecType"
    Field <- "PresentUse"
  }
 
+# 2.0 Read in Data  ------------------------------------------------------------
+
+ # 2.1 Start Loop
 for(j in 1:length(Ys)){
 
+ # 2.2 Read Data and Merge to PX
  odbc <- odbcConnectAccess2007(paste0(
     "C://Dropbox//Data//WA//King//Assessor//Annual//King", Ys[j], ".accdb"))
  
@@ -27,12 +50,16 @@ for(j in 1:length(Ys)){
  odbcClose(odbc)
  } # Closes J loop
     
-# Correct Vals
+# 3.0 Correct Time Values ------------------------------------------------------
+
+# 3.1 Correct Vals
   PX[is.na(PX)]<- -.1
   for(Q in 1:yl){
    zzz <- c(as.numeric(PX[Q, 2]), as.numeric(PX[Q, 2:(length(Ys))]))
    zzzz <- zzz - as.numeric(PX[Q, 2:(length(Ys) + 1)])
    Results[Q] <- Ys[which(abs(zzzz) == max(abs(zzzz)))[1]]
   }
+
+# 4.0 Return Results -----------------------------------------------------------
 return(Results)
 }
