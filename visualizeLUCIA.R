e461edf04b53b580f3bc2c7eaa723ffceb93c4eb
Andy Krause <alkrause@uw.edu>
2013-11-14 09:19:05 -0800

    visualizeLUCIA: Add rgl 2d function

--- visualizeLUCIA.R
+++ visualizeLUCIA.R
@@ -13,12 +13,19 @@
 # 1.0 Base Plotting of Shapefiles ----------------------------------------------
 
 basePlot <- function(parcels, clip, bor.col, fill.col, lWd, toAdd = FALSE){
+
+  # Clip to desired area
   par.clip <- selectByArea(parcels, clip)
   
+  # Remove Road parcels, if any
+  par.clip <- par.clip[substr(par.clip@data$PINX, 9, 12) != "5555", ] 
+  
+  # If adding to existing
   if(toAdd){
    plot(par.clip, bor=bor.col, col=fill.col, lwd=lWd, add=T)
   }
   
+  # If creating new
   if(!toAdd){
     plot(par.clip, bor=bor.col, col=fill.col, lwd=lWd, add=T)
   }
@@ -30,15 +37,25 @@ basePlot <- function(parcels, clip, bor.col, fill.col, lWd, toAdd = FALSE){
 
 
 # 3.0 Plot Shapefiles in RGL ---------------------------------------------------
-# 
-# rgl2dPlot <- function(parcel, height, color, lWd){
-#   
-#   
-# 
-# rgl.shapefile(par.beg, 0, "gray80", 1)
-# 
+ 
+rgl2dPlot <- function(parcel, height, r.col, lWd, toAdd=FALSE){
+  
+  # Remove road parcels, if any
+  parcel <- parcel[substr(parcel@data$PINX, 9, 12) != "5555", ] 
+  
+  
+  # If adding to existing
+  if(toAdd){   
+    rgl.shapefile(parcel, height, r.col, lWd)
+  }
+ 
+  # If creating new
+  if(!toAdd){
+    rgl.open()   
+    rgl.shapefile(parcel, height, r.col, lWd)
+  }
 
-
+# 4.0 Plot Structures in RGL ---------------------------------------------------
 
 
 
