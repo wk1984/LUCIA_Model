ebb28d6ed0d7884b338b82831c94b4cc82ea589f
Andy Krause <alkrause@uw.edu>
2013-11-17 12:54:58 -0800

    LUCIA_RunShinyApp: Initial Commit for file that manages the Shiny App locations

---
+++ LUCIA_RunShinyApp.R
@@ -0,0 +1,15 @@
+
+library(shiny)
+
+rstudio::viewer("http://localhost:8100")
+
+# Within R Studio
+
+runApp("D://Code//R//Research//LUCIA_Model",launch.browser = rstudio::viewer)
+
+
+# Within Web App
+runApp("D://Code//R//Research//LUCIA_Model")
+
+
+ 
