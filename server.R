0b81a483e101c9cc2cba04073241e859aae0598d
Andy Krause <alkrause@uw.edu>
2013-11-17 12:53:58 -0800

    server.R:  Fix issue with calling wrong workspace

--- server.R
+++ server.R
@@ -12,11 +12,18 @@
 library(shiny)
 library(simPH)
 library(sp)
+library(RODBC)
+library(maptools)
+library(rgeos)
+library(stringr)
+library(rgl)
 
 # load in necessary data
-
+ 
 save.image(paste0("D://Data//Research//Dissertation//LUCIA_Paper//",
-                  "Seattle_LUCIA_Workspace.RData"))
+                   "Seattle_LUCIA_Shiny_Workspace.RData"))
+
+source("D://Code//R//Research//LUCIA_Model//visualizeLUCIA.R")
 
 # start server
 
@@ -24,7 +31,7 @@ shinyServer(function(input, output) {
 
  # Set Text  
   formulaText <- reactive({
-    paste0("Change Map for ", input$variable, " Urban Village")
+    paste0("Change Map for ", input$urbanvillage, " Urban Village")
   })
   
  # Set output caption  
@@ -32,12 +39,17 @@ shinyServer(function(input, output) {
     formulaText()
   })
  
- # Create output plot  
+ #Create output plot  
   output$Change.Plot <- renderPlot({
     
-  # Get data/model from inputs
-    x.model <- get(input$model)
-    x.variable <- redev.data[ ,input$variable]
-
+    clip <- UVs[UVs@data$Code == input$urbanvillage,]
+    
+    Change.Table <- tabulateChanges(clip, beg.parcels, end.parcels, Par.Hist,
+                                    Beg.Year, End.Year, CType="Raw")
+    
+    changePlot(end.parcels, clip, Change.Table, "Gain.Units",
+               c(5, 25, 100), c(3, 4, 5), c(15, 15, 15), 3)
+    plot(clip, lwd=2, bor=1, add=T)
+    
   })
 })
