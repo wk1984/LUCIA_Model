b38bc3e6d68942b255c7185ffe0d2d07ed6fcd08
Andy Krause <alkrause@uw.edu>
2013-11-30 06:51:22 -0800

    server.R:  Add Legend

--- server.R
+++ server.R
@@ -20,10 +20,13 @@ library(rgl)
 
 # load in necessary data
  
-save.image(paste0("D://Data//Research//Dissertation//LUCIA_Paper//",
+load(paste0("D://Data//Research//Dissertation//LUCIA_Paper//",
                    "Seattle_LUCIA_Shiny_Workspace.RData"))
 
+source("D://Code//R//Research//LUCIA_Model//tabulateChanges.R")
 source("D://Code//R//Research//LUCIA_Model//visualizeLUCIA.R")
+source("D://Code//R//General//Toolsets//rgl_SpatialTools.R")
+source("D://Code//R//General//R_Helpers//Spatial_Helpers.R")
 
 # start server
 
@@ -43,13 +46,20 @@ shinyServer(function(input, output) {
   output$Change.Plot <- renderPlot({
     
     clip <- UVs[UVs@data$Code == input$urbanvillage,]
+    clip.bb <- clip@bbox
+    
     
     Change.Table <- tabulateChanges(clip, beg.parcels, end.parcels, Par.Hist,
                                     Beg.Year, End.Year, CType="Raw")
     
-    changePlot(end.parcels, clip, Change.Table, "Gain.Units",
+#     plot(c(clip.bb[1,1], clip.bb[1,2]), c(clip.bb[2,1], clip.bb[2,2]),col=0,
+#          xaxt="n", yaxt="n", xlab="", ylab="")
+      changePlot(end.parcels, clip, Change.Table, "Gain.Units",
                c(5, 25, 100), c(3, 4, 5), c(15, 15, 15), 3)
-    plot(clip, lwd=2, bor=1, add=T)
+    
+      legend("topleft", c("1-5 Units", "6-25 Units", "26+ Units"),
+             col=c(3,4,5), pch=15, cex=.9)
+      plot(clip, lwd=2, bor=1, add=T)
     
   })
 })
